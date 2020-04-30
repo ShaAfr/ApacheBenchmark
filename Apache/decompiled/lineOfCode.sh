def235b4bd262efa3aa2702d9a9371d158b5ce2c
Sharmin Afrose <sharminafrose@vt.edu>
2020-04-30 16:50:33 -0400

    decompiled

---
+++ Apache/decompiled/lineOfCode.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+
+printf "\nProjectBoundaryForAnalysis-01\n"
+cloc activemq-artemis/artemis-commons-2.5.0-SNAPSHOT_source_from_Procyon.zip
+printf "\nProjectBoundaryForAnalysis-02\n"
+cloc deltaspike/deltaspike-core-impl-1.8.1-SNAPSHOT_source_from_Procyon.zip
+printf "\nProjectBoundaryForAnalysis-03\n"
+cloc directory-server/apacheds-kerberos-codec-2.0.0-M25-SNAPSHOT_source_from_Procyon.zip
+printf "\nProjectBoundaryForAnalysis-04\n"
+cloc incubator-taverna-workbench/taverna-perspective-myexperiment-3.1.0-incubating-SNAPSHOT_source_from_Procyon.zip
+printf "\nProjectBoundaryForAnalysis-05\n"
+cloc manifoldcf/mcf-core-2.9-SNAPSHOT_source_from_Procyon.zip
+printf "\nProjectBoundaryForAnalysis-06\n"
+cloc meecrowave/meecrowave-core-1.2.0-SNAPSHOT_source_from_Procyon.zip
+printf "\nProjectBoundaryForAnalysis-07\n"
+cloc spark/spark-core_2.11-2.4.0-SNAPSHOT_source_from_cfr.zip
+printf "\nProjectBoundaryForAnalysis-08\n"
+cloc tika/tika-core-1.17-SNAPSHOT_source_from_Procyon.zip
+printf "\nProjectBoundaryForAnalysis-09\n"
+cloc tomee/openejb-core-7.0.5-SNAPSHOT_source_from_Procyon.zip
+printf "\nProjectBoundaryForAnalysis-10\n"
+cloc wicket/wicket-util-8.0.0-SNAPSHOT_source_from_Procyon.zip
+
+
+
+
+
